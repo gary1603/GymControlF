19ecce2c8933f43cc2f9bed763480e7e4052c01b
Gary Sequeira <Gary Sequeira@DESKTOP-3HP58V7>
2025-04-19 16:22:09 -0600

    prueba completa

--- ControlProyecto/Program.cs
+++ ControlProyecto/Program.cs
@@ -61,7 +61,7 @@ public class Program
         string correo = ValidarEntrada();
         Console.Write("Teléfono: ");
         string telefono = ValidarEntrada();
-        // prueba 5
+        // prueba commit
         using (SqlConnection conn = new SqlConnection(connectionString))
         {
             string query = "INSERT INTO Usuarios (id_usuario,nombre, apellido, fecha_nacimiento, genero, correo, telefono) " +
