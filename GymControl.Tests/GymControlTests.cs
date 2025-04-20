d1c0e54b7883d82ca2a8e67c6c901444bd6c395f
Gary Sequeira <Gary Sequeira@DESKTOP-3HP58V7>
2025-04-19 18:54:46 -0600

    .

--- GymControl.Tests/GymControlTests.cs
+++ GymControl.Tests/GymControlTests.cs
@@ -8,7 +8,7 @@ namespace GymControl.Tests
     {
         // Cadena de conexión para la base de datos de pruebas
       //  private static string connectionString = "data source=DESKTOP-3HP58V7\\SQLEXPRESS;initial catalog=GymControl;trusted_connection=true;";
-        private static string connectionString = "data source=DESKTOP-3HP58V7\\SQLEXPRESS;initial catalog=GymControl;user id=usuario;password=12345;";
+        private static string connectionString = "data source=tcp:DESKTOP-3HP58V7\\SQLEXPRESS;initial catalog=GymControl;user id=usuario;password=12345;";
 
 
 
